4581e7ab0b8cedf1b555bc003b570d40e5df9c86
drkhnntstsgg <adarkhantsetseg48@gmail.com>
2025-01-29 14:28:36 +0800

    dasgal

---
+++ dsgl1.py
@@ -0,0 +1,8 @@
+course_id = input("Хичээлийн код: ")
+course_name = input("Хичээлийн нэр: ")
+credit = input("Хичээлийн кредит: ")
+
+madlib = " Энэ хичээлийн код нь {}".format(course_id) + \
+", хичээлийн нэр нь {}".format(course_name).title() + \
+"  бөгөөд уг хичээл нь {}".format(credit) + " кредийн хичээл юм."
+print(madlib)
